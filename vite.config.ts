d87db5c91bc79c75cd7dc93b696802f93d68123f
sassykeyah <saskiaday@hotmail.com>
2024-09-04 11:38:26 +1000

    yes

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/timelineproject/',
   plugins: [
     vue(),
     vueJsx(),
